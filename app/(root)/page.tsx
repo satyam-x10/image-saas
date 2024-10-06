4fefcca97aac3ad7e1b68f276de47c76d8157c72
satyam kumar <satyamx10@gmail.com>
2024-10-06 04:47:27 -0800

    redo

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,10 +5,10 @@ import Image from "next/image"
 import Link from "next/link"
 
 const Home = async ({ searchParams }: SearchParamProps) => {
-  const page = Number(searchParams?.page) || 1;
-  const searchQuery = (searchParams?.query as string) || '';
+  // const page = Number(searchParams?.page) || 1;
+  // const searchQuery = (searchParams?.query as string) || '';
 
-  const images = await getAllImages({ page, searchQuery})
+  // const images = await getAllImages({ page, searchQuery})
 
   return (
     <>
@@ -32,14 +32,14 @@ const Home = async ({ searchParams }: SearchParamProps) => {
         </ul>
       </section>
 
-      <section className="sm:mt-12">
+      {/* <section className="sm:mt-12">
         <Collection 
           hasSearch={true}
           images={images?.data}
           totalPages={images?.totalPage}
           page={page}
         />
-      </section>
+      </section> */}
     </>
   )
 }
